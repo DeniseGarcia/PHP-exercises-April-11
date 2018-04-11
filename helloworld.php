92c3d712fb2cdf68d014c71f86538d399535bf7b
Denise Garcia <35517622+DeniseGarcia@users.noreply.github.com>
2018-04-11 15:16:23 +0800

    Add files via upload

---
+++ helloworld.php
@@ -0,0 +1,22 @@
+<html>
+	<head>
+		<title>Hello World Script</title>
+	</head>
+
+	<body>
+		<?php
+			
+			echo 123;
+			//echo"Hello World!<br>";
+			echo"Hello World!<br>";
+			echo"Hello"," World!<br>";
+			//echo"Hello World!<br>";
+			//echo"Hello World! , Hello World!<br>";	
+		?>
+
+	</body>
+
+
+
+</html>
+
