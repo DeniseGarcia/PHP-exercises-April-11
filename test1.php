f3d7aa0e878167765267ac21bb9dc16895babef0
Denise Garcia <35517622+DeniseGarcia@users.noreply.github.com>
2018-04-11 15:38:57 +0800

    Add files via upload

---
+++ test1.php
@@ -0,0 +1,9 @@
+<?php
+	$color="blue";
+	//$color="red";
+	//$color="blue";
+	echo $color; echo "<br>";
+	$color="red";
+	echo $color;
+
+?>
---
+++ test2.php
@@ -0,0 +1,5 @@
+<?php
+	$num=55.27;
+	printf("The conversion rate of dollar to peso is %f", $num);
+
+?>
